b92c54edddad46aeabc19f8a222b62c26b09bb6f
chep1126 <772575249@qq.com>
2017-09-12 11:48:20 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -29,29 +29,4 @@ def send_msg(Subject,msg):
 if __name__=="__main__":
     send_msg("这是一封测试邮件的主题","这是一封测试邮件的内容")
 
-# # 第三方 SMTP 服务
-# mail_host="smtp.qq.com"  #设置服务器
-# mail_user="772575249@qq.com"    #用户名
-# mail_pass="xbjdncgiajdgbbeh"   #口令
-#
-#
-# sender = '772575249@qq.com'
-# receivers = ['15008487521@163.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
-#
-# message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-# message['From'] = Header("菜鸟教程", 'utf-8')
-# message['To'] =  Header("测试", 'utf-8')
-#
-# subject = 'Python SMTP 邮件测试'
-# message['Subject'] = Header(subject, 'utf-8')
-#
-#
-# try:
-#     smtpObj = smtplib.SMTP()
-#     smtpObj.connect(mail_host, 25)    # 25 为 SMTP 端口号
-#     smtpObj.login(mail_user,mail_pass)
-#     smtpObj.sendmail(sender, receivers, message.as_string())
-#     print ("邮件发送成功")
-# except smtplib.SMTPException as e:
-#
-#     print (e)
+
